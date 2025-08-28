1a2ea9c1bc066124c8c1b4c150111d2b70be3075
abhikhya2024 <abhikhya2024>
2025-08-28 15:43:45 +0530

    celery logging

--- myproject/myproject/celery.py
+++ myproject/myproject/celery.py
@@ -1,13 +1,18 @@
 import os
 from celery import Celery
+import logging.config
+from django.conf import settings
 
 # Set default Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 
 app = Celery("myproject")
 
-# Load settings with "CELERY_" prefix from settings.py
+# Load settings with "CELERY_" prefix
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Auto-discover tasks.py inside installed apps
+# Auto-discover tasks
 app.autodiscover_tasks()
+
+# Load Django logging into Celery worker
+logging.config.dictConfig(settings.LOGGING)
