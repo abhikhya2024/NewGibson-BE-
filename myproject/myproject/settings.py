f19fad5cfc87c0bda10bf2813a330dfbe76c91f6
abhikhya2024 <abhikhya2024>
2025-09-01 16:34:14 +0530

    db corrected name

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ruck',
+        'NAME': 'ruckd',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
