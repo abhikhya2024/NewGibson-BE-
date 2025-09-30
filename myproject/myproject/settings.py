5bb07be70cec485ec86d613539fdac255fb6d069
abhikhya2024 <abhikhya2024>
2025-09-30 17:30:15 +0530

    change

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = '/static/'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
