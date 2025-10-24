6eac2d6ff41114dae1713497c1feabe0fd7c191c
abhikhya2024 <abhikhya2024>
2025-10-24 22:10:01 +0530

    logging

--- myproject/project/sharepoint_utils.py
+++ myproject/project/sharepoint_utils.py
@@ -696,18 +696,19 @@ def search_documents(ix, q_text_field_map, mode="fuzzy", max_edits=2, join_with=
         field_queries = []
         for text, fields in q_text_field_map.items():
             q = make_query(text, fields)
-            logger.info("qqqqqqqqqqqqqqqqqqqqq00000000000000000", q)
+            logger.info("qqqqqqqqqqqqqqqqqqqqq00000000000000000", text, fields)
             if q is not None:
                 field_queries.append(q)
+            logger.info("qqqqqqqqqqqqqqqqqqqqq00000000000000000", field_queries)
 
         if not field_queries:
             final_query = Every()
-            logger.info("qqqqqqqqqqqqqqqqqqqqq11111111111111111", q)
+            logger.info("qqqqqqqqqqqqqqqqqqqqq11111111111111111", final_query)
 
         else:
             # ✅ AND across all query groups
             final_query = And(field_queries)
-            logger.info("qqqqqqqqqqqqqqqqqqqqq222222222222222222", q)
+            logger.info("qqqqqqqqqqqqqqqqqqqqq222222222222222222", final_query)
 
         # -------- PAGINATED SEARCH --------
         try:
