80db16f3c3dcc597aba9110cae25ef2e361ac238
abhikhya2024 <abhikhya2024>
2025-11-12 19:29:13 +0530

    dab changes to farrar

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farrar3',
+        'NAME': 'farrar',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
