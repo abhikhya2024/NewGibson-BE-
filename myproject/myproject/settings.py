6f08dcf8d34b727075d4d5252d8b68503d53cf77
abhikhya2024 <abhikhya2024>
2025-11-12 17:22:54 +0530

    db changed again

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'farrar2',
+        'NAME': 'farrar3',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': 'localhost',
